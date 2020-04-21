1ec59d8cde800026f67e6aa7acd3fcdc61140b22
diegoncollazo <diego.nicolas@outlook.com.ar>
2020-04-21 18:25:03 -0300

    UTN

---
+++ Cuatrimestre II/Cursada/2020/Primera parte/Lavadero/ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto auto = new Auto(5, "DWR191", 4, Vehiculo.EMarcas.Honda);
+            Moto moto = new Moto(300, "A036PKH", 2, Vehiculo.EMarcas.Honda);
+            Camion camion = new Camion(2, "DNC117", 6, Vehiculo.EMarcas.Iveco);
+
+            Console.WriteLine(auto.MostrarAuto());
+            Console.WriteLine(moto.MostrarMoto());
+            Console.WriteLine(camion.MostrarCamion());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Cuatrimestre II/Cursada/2020/Primera parte/Lavadero/Entidades/Lavadero.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Lavadero
+    {
+        public enum EVehiculos
+        {
+            Auto, Moto, Camion
+        }
+
+        private List<Vehiculo> vehiculos;
+        private float precioAuto;
+        private float precioMoto;
+        private float precioCamion;
+
+        public string Info
+        {
+            get
+            {
+                StringBuilder retorno = new StringBuilder();
+                foreach (Vehiculo item in this.Vehiculos)
+                {
+                    if (item is Auto)
+                        retorno.AppendLine(((Auto)(item)).MostrarAuto());
+                    else if (item is Camion)
+                        retorno.AppendLine(((Camion)(item)).MostrarCamion());
+                    else
+                        retorno.AppendLine(((Moto)(item)).MostrarMoto());
+                }
+                return retorno.ToString();
+            }
+        }
+        public List<Vehiculo> Vehiculos
+        {
+            get
+            {
+                return this.vehiculos;
+            }
+        }
+
+        private Lavadero()
+        {
+            this.vehiculos = new List<Vehiculo>();
+        }
+        public Lavadero(float precioAuto, float precioMoto, float precioCamion)
+        {
+            this.precioAuto = precioAuto;
+            this.precioCamion = precioCamion;
+            this.precioMoto = precioMoto;
+        }
+
+        public double MostrarTotalFacturado()
+        {
+            double retorno = 0;
+            retorno = MostrarTotalFacturado(EVehiculos.Auto) + MostrarTotalFacturado(EVehiculos.Camion) + MostrarTotalFacturado(EVehiculos.Moto);
+            return retorno;
+        }
+        public double MostrarTotalFacturado(EVehiculos vehiculo)
+        {
+            double retorno = 0;
+            foreach (Vehiculo item in this.Vehiculos)
+            {
+                switch (vehiculo)
+                {
+                    case EVehiculos.Auto:
+                        retorno += this.precioAuto;
+                        break;
+                    case EVehiculos.Camion:
+                        retorno += this.precioCamion;
+                        break;
+                    case EVehiculos.Moto:
+                        retorno += this.precioMoto;
+                        break;
+                }
+            }
+            return retorno;
+        }
+
+        public static bool operator ==(Lavadero lavadero, Vehiculo vehiculo)
+        {
+            bool retorno = false;
+            foreach (Vehiculo item in lavadero.Vehiculos)
+            {
+                if (item == vehiculo)
+                {
+                    retorno = true;
+                    break;
+                }
+            }
+            return retorno;
+        }
+        public static bool operator !=(Lavadero lavadero, Vehiculo vehiculo)
+        {
+            return !(lavadero == vehiculo);
+        }
+
+    }
+}
