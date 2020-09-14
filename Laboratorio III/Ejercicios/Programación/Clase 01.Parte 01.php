956bbfcb2054661967c572a62a9711e09fae63ec
diegoncollazo <diego.nicolas@outlook.com.ar>
2020-09-14 03:09:57 -0300

    UTN

--- Laboratorio III/Ejercicios/Programación/Clase 01.Parte 01.php
+++ Laboratorio III/Ejercicios/Programación/Clase 01.Parte 01.php
@@ -25,24 +25,24 @@ do {
     if ($total + $contador <= 1000) {
         $total += $contador;
         $contador++;
-    }
-    else{
+    } else {
         break;
     }
-}while (true);
+} while (true);
 echo 'Suma total: '.$total.'<br/>';
 echo 'Cantidad de enteros: '.$contador.'<br/>';
 
 echo '<br/>Ejercio 05.<br/>';
 $a = 5; $b = 9; $c = 8;
-if ($a > $b && $b < $c && $a != $c)
+if ($a > $b && $b < $c && $a != $c) {
     echo 'Es B.<br/>';
-else if ($b > $c && $c < $a && $b != $a)
+} elseif ($b > $c && $c < $a && $b != $a) {
     echo 'Es C.<br/>';
-else if ($b > $a && $a < $c && $b != $c)
+} elseif ($b > $a && $a < $c && $b != $c) {
     echo 'Es A.<br/>';
-else
+} else {
     echo 'No hay valor del medio.<br/>';
+}
 
 echo '<br/>Ejercio 06.<br/>';
 $operador = '-';
@@ -72,5 +72,34 @@ echo '<br/>Ejercio 07.<br/>';
 // Imprime: July 1, 2000 is on a Saturday
 echo "July 1, 2000 is on a " . date("l", mktime(0, 0, 0, 7, 1, 2000));
 
+echo '<br/>Ejercio 08.<br/>';
+function number_suffix($number)
+{
+    // Validate and translate our input
+    if (is_numeric($number)) {
+        // Get the last two digits (only once)
+        $n = $number % 100;
+    } else {
+        // If the last two characters are numbers
+        if (preg_match('/[0-9]?[0-9]$/', $number, $matches)) {
+            // Return the last one or two digits
+            $n = array_pop($matches);
+        } else {
+            // Return the string, we can add a suffix to it
+            return $number;
+        }
+    }
+    // Skip the switch for as many numbers as possible.
+    if ($n > 3 && $n < 21) {
+        return $number . 'th';
+    }
 
-?>
+    // Determine the suffix for numbers ending in 1, 2 or 3, otherwise add a 'th'
+    switch ($n % 10) {
+      case '1': return $number . 'st';
+      case '2': return $number . 'nd';
+      case '3': return $number . 'rd';
+      default:  return $number . 'th';
+    }
+}
+  echo number_suffix(24);
--- Laboratorio III/Ejercicios/Programación/Clase 01.Parte 02.php
+++ Laboratorio III/Ejercicios/Programación/Clase 01.Parte 02.php
@@ -1,4 +1,23 @@
 <?php
-    $vector = array([5]);
-    echo $vector;
-?>
+    $numero = 39;
+    echo gettype($numero);
+    echo '</br>';
+    echo var_dump($numero);
+    echo '</br>';
+
+    $texto = '"Edad: '.$numero.'"</br>';
+    echo $texto;
+
+    $texto = "\"Edad: $numero \"</br>";
+    echo $texto;
+
+    // Constantes
+    define('nombre', 'Diego');
+    echo nombre.'</br>';
+
+    // Constantes predefinidas
+    echo PHP_OS.'</br>';
+    echo PHP_VERSION.'</br>';
+    echo __LINE__.'</br>';
+
+    
